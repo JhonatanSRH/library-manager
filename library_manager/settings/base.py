360d18f85ed1ef9e300a1e2da5074d1552f8bb03
Jhonatan Rodriguez <jhonstiven99@hotmail.com>
2025-01-17 17:40:54 -0500

    FIX: agregados archivos estaticos

--- library_manager/settings/base.py
+++ library_manager/settings/base.py
@@ -101,7 +101,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 
